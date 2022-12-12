766bbc3d2249bf3124da483b085fa940144d8dfd
Bartek Debiec <bartekdbc@gmail.com>
2022-12-12 13:23:12 +0800

    styled components implemented to Buttons component

---
+++ src/Buttons/styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Button = styled.button`
+  background: transparent;
+  color: hsl(187, 100%, 25%);
+  border: none;
+  font-size: 18px;
+  text-align: center;
+  max-height: 40px;
+  cursor: pointer;
+  transition: color 0.3s;
+
+  @media (max-width: 767px) {
+    flex-basis: 100%;
+    margin: 10px;
+  }
+
+  &:hover {
+    color: hsl(187, 100%, 35%);
+  }
+
+  &:active {
+    color: hsl(187, 100%, 45%);
+  }
+
+  &:disabled {
+    color: hsl(0, 4%, 65%);
+  }
+`;
