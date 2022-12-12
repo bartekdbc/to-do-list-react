e1176a1f09d7ca14acb349679295a83e1efbca32
Bartek Debiec <bartekdbc@gmail.com>
2022-12-12 18:58:15 +0800

    theme.js implemented with colors and breakpoint

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    color: {
+        teal: "#007180",
+        silver: "#A9A2A2",
+        black: "#000000",
+        white: "#FFFFFF",
+        gallery: "#EEEEEE",
+        alto: "#DDDDDD",
+        red: "#FF0000",
+        forestGreen: "#228C25",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    }
+};
