f23e8b3f37213be31e2ec636a2b99b26b052f198
Bartek Debiec <bartekdbc@gmail.com>
2022-12-25 21:04:25 +0800

    appliaction split into features

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { theme } from "./theme";
 
-import { configureStore } from "@reduxjs/toolkit";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
