70d5eb210be817942167527f9d40176dd1b85c9a
Bartek Debiec <bartekdbc@gmail.com>
2022-12-27 13:48:39 +0800

    formatted Tasks component

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,37 +1,20 @@
-import { useSelector } from "react-redux";
 import Form from "../tasks/Form";
 import TaskList from "../tasks/TaskList";
 import Buttons from "../tasks/Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import { Container } from "../../common/Container/styled";
-import { useTasks } from "../../useTasks";
-import { selectTasks } from "./tasksSlice";
 
 function Tasks() {
-
-  const { tasks } = useSelector(selectTasks);
-
-  const {
-    // tasks,
-  } = useTasks();
-
-  return (
+   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        body={<Form />}
-      />
+      <Section title="Dodaj nowe zadanie" body={<Form />} />
 
       <Section
         title="Lista zadań"
-        body={
-          <TaskList />
-        }
-        extraContent={
-          <Buttons />
-        }
+        body={<TaskList />}
+        extraContent={<Buttons />}
       />
     </Container>
   );
