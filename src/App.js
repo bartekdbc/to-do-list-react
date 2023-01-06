c8ed1687a2f7eab8cb5d616207cb105a25ef2f22
Bartek Debiec <bartekdbc@gmail.com>
2023-01-06 15:30:52 +0800

    navigation implemented

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/author/AuthorPage";
 
-export default () => <Tasks />;
+const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+        <li>
+          <Link to="/autor">O autorze</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania/:id">
+          <TaskPage />
+        </Route>
+        <Route path="/zadania">
+          <TasksPage />
+        </Route>
+        <Route path="/autor">
+          <AuthorPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/zadania" />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
+
+export default App;
