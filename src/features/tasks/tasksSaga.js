d203062cafd41ff6c91064219242a24660f881ab
Bartek Debiec <bartekdbc@gmail.com>
2023-01-06 09:59:05 +0800

    generator function renamed to tasksSaga

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -25,7 +25,7 @@ function* saveTasksInLocalStorageHandler() {
   yield call(saveTasksInLocalStorage, tasks);
 }
 
-export function* watchFetchExampleTasks() {
+export function* tasksSaga() {
   yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
   yield takeEvery("*", saveTasksInLocalStorageHandler);
 }
